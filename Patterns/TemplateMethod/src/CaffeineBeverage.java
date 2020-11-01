979b660dd2e7486035293c35355fd0e7f7e0a4f5
Nick <omegaphenix@mail.ru>
2020-11-01 16:27:50 +0300

    Template Method

---
+++ Patterns/TemplateMethod/src/CaffeineBeverage.java
@@ -0,0 +1,24 @@
+public abstract class CaffeineBeverage {
+
+
+    final void prepareRecipe(){//Шаблонный метод
+        boilWater();
+        brew();
+        pourInCup();
+        if (customerWantsCondiments()){
+            addCondiments();
+        }
+
+    }
+    abstract void brew();
+    abstract void addCondiments();
+        void boilWater(){
+            System.out.println("Boiling water");
+        }
+        void pourInCup(){
+            System.out.println("Pouring into cup");
+        }
+        boolean customerWantsCondiments(){
+            return true;
+        }
+}
